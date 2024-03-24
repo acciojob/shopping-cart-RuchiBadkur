aab6010878159d0d62815a0d83457c20c89d0adc
Ruchi Badkur <94377483+RuchiBadkur@users.noreply.github.com>
2024-03-24 22:54:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
 //your code here
+document.getElementById('add').addEventListener('click', function() {
+    var itemName = document.getElementById('item-name-input').value;
+    var itemPrice = parseFloat(document.getElementById('item-price-input').value);
+
+    if (itemName && !isNaN(itemPrice) && itemPrice > 0) {
+        var newRow = document.createElement('tr');
+        var itemNameCell = document.createElement('td');
+        itemNameCell.textContent = itemName;
+        var itemPriceCell = document.createElement('td');
+        itemPriceCell.textContent = itemPrice.toFixed(2);
+        newRow.appendChild(itemNameCell);
+        newRow.appendChild(itemPriceCell);
+        document.getElementById('cart-items').insertBefore(newRow, document.getElementById('grand-total-row'));
+
+        var total = parseFloat(document.getElementById('grand-total').textContent);
+        total += itemPrice;
+        document.getElementById('grand-total').textContent = total.toFixed(2);
+
+        document.getElementById('item-name-input').value = '';
+        document.getElementById('item-price-input').value = '';
+    }
+});
